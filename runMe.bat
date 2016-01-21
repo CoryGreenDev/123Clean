55a740c88f8adbb7720001bfd4ec4a70cca2b511
Cory Green <1elitehacker@gmail.com>
2016-01-21 15:49:41 -0600

    Create runMe.bat
    v1

---
+++ runMe.bat
@@ -0,0 +1,29 @@
+@echo off
+echo /###########################################################\
+echo [                 123Clean - Cory Green                     ]
+echo [                         ~v.1                              ]
+echo [                    ~Apache License~                       ]
+echo [ WARNING: ALL FILES IN TEMP & DOWNLOADS WILL BE DELETED.   ]
+echo [        If you don't know what that is, don't worry.       ]
+echo [       Click the [X] if you need to quit, other wise       ]
+echo \###########################################################/
+pause
+
+echo Please Wait...
+
+cd %temp%
+del %temp%
+
+
+echo Cleaned directory TEMP.
+echo Starting to clear Download folder in 5 seconds, quit now if you do NOT want this to be deleted.
+ping localhost -n 5
+cls
+echo What is your User Account name (NO Captialization) (Also this might take a long time, depending on the size.)
+set /p user=>>: 
+
+del C:/Users/%user%/Downloads
+
+echo Cleaning Complete, Thanks for using 123Clean! Press any key to quit!
+pause >nul
+exit
