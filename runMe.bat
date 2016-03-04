bde02d083e959837d45ea0770dd4fffd19ddd626
Cory Green <1elitehacker@gmail.com>
2016-03-04 13:13:14 -0500

    v4.5

--- runMe.bat
+++ runMe.bat
@@ -14,7 +14,7 @@ echo ----------------------------------------
 ping localhost -n 2 >nul
 set/a loadnum=%loadnum% +1
 if %loadnum%==20 goto Done
-rem You can set the number of ()'s as whatever you rem want but remember: in your "loading box" you rem need 2 spaces for every () because "()" takes up rem 2 spaces. The above box has 40 spaces, so rem twenty repeats, adding 1 () every repeat.
+rem You can set the number of ()'s as whatever you want but remember: in your "loading box" you need 2 spaces for every () because "()" takes up 2 spaces. The above box has 40 spaces, so twenty repeats, adding 1 () every repeat.
 goto Loading
 :Done
 echo.
